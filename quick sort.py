48aea9a8e100eae9e692c90d2c291a683e62ec65
kshibarn <73215352+kshibarn@users.noreply.github.com>
2021-01-18 00:21:59 +0530

    Add files via upload

---
+++ quick sort.py
@@ -0,0 +1,19 @@
+def partition(a,low,high):
+    i = ( low-1 )
+    pivot = a[high]
+    for j in range(low , high):
+        if   a[j] <= pivot:
+            i = i+1
+            a[i],a[j] = a[j],a[i]
+    a[i+1],a[high] = a[high],a[i+1]
+    return ( i+1 )
+def quickSort(a,low,high):
+    if low < high:
+        pi = partition(a,low,high)
+        quickSort(a, low, pi-1)
+        quickSort(a, pi+1, high)
+n=int(input("enter no of elements:"))
+a=[int(input()) for i in range(n)]
+quickSort(a,0,n-1)
+print ("Sorted array is:")
+print(a)
